c49986c53b4255545caeb2e8ed11db2e4ec5757a
Pablo Endara-Santiago <litotechenterprise@gmail.com>
2023-07-11 11:48:30 -0400

    added Upload page

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 interface LayoutProps {
   children?: React.ReactNode;
 }
@@ -8,9 +10,16 @@ export default function Layout({ children }: LayoutProps) {
       <header className="container sticky top-0 z-40 bg-white">
         <div className="h-16 border-b border-b-slate-200 py-4">
           <nav className="ml-4 pl-6">
-            <a href="#" className="hover:text-slate-600 cursor-pointer">
+            <Link href="/" className="hover:text-slate-600 cursor-pointer px-4">
               Home
-            </a>
+            </Link>
+
+            <Link
+              href="/upload"
+              className="hover:text-slate-600 cursor-pointer "
+            >
+              Upload
+            </Link>
           </nav>
         </div>
       </header>
